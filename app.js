2bb114e1351b28d0fec700fac116d9cc5159e69e
Sayrarm <minsecond@mail.ru>
2025-12-08 23:19:40 +0300

    feat: add animation for burger button

--- app.js
+++ app.js
@@ -128,3 +128,21 @@ document.addEventListener('DOMContentLoaded', function() {
         isRub = !isRub;
     });
 });
+
+/*бургер меню*/
+
+let modal = document.getElementById('modal_nav');
+let burgerBtn = document.getElementById('burger');
+
+burgerBtn.addEventListener('click', function() {
+    if (modal.classList.contains('active')) {
+        // Закрываем с анимацией
+        modal.classList.add('closing');
+        setTimeout(() => {
+            modal.classList.remove('active', 'closing');
+        }, 200);
+    } else {
+        // Открываем
+        modal.classList.add('active');
+    }
+});
