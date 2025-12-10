5c04d5d08cf82759f49ea2ebb2877e880fc1c2b8
Sayrarm <minsecond@mail.ru>
2025-12-10 13:50:28 +0300

    fix: fix burger menu for all pages

---
+++ burger.js
@@ -0,0 +1,17 @@
+/*бургер меню*/
+
+let modal = document.getElementById('modal_nav');
+let burgerBtn = document.getElementById('burger');
+
+burgerBtn.addEventListener('click', function() {
+    if (modal.classList.contains('active')) {
+        // Закрываем с анимацией
+        modal.classList.add('closing');
+        setTimeout(() => {
+            modal.classList.remove('active', 'closing');
+        }, 200);
+    } else {
+        // Открываем
+        modal.classList.add('active');
+    }
+});
